7ce8b155d7c4a51538eee5376736fb5daf6045c8
leondelee <leevecent@outlook.com>
2018-10-02 20:56:45 +0800

    updates

--- cs/ass2_find_all_factors.c
+++ cs/ass2_find_all_factors.c
@@ -1,47 +1,33 @@
 #include<stdio.h>
 #include<math.h>
 
-int find_num_of_factors(long int N);
-int is_prime(long int num);
+long long int find_num_of_factors(long long int N);
 
 int main(int argc, char const *argv[])
 {
-    char one_ch;
-    while(scanf("%c", &one_ch) != EOF) {
-        long int N = 0;
-        if(one_ch != '\n') {
-            N = N * 10;
-            N += (int) one_ch;
-        }
-        printf("%d", find_num_of_factors(N));
+    // int one_ch;
+    long long int N = 0;
+    long long int res = 0;
+    while(scanf("%lld", &N) != EOF) {
+        res = find_num_of_factors(N);
+        printf("%lld\n", res);
+        N = 0;
+        res = 0;
     }
-    // printf("%d", is_prime(N));
     return 0;
 }
 
-int find_num_of_factors(long int N) {
-    int res_count = 1;
-    double N_ = N;
-    for(long int fc = 1; fc <= sqrt(N_) + 1; fc ++){
-        if(is_prime(fc)) {
-            int count = 1;
-            while(N % fc == 0) {
-            count ++;
-            N = N / fc;
-            }
-            res_count = res_count * count;
+long long int find_num_of_factors(long long int N) {
+    if(N == 1) return 1;
+    else if(N == 2) return 2;
+    else if(N == 3) return 2;
+    else if(N == 4) return 3;
+    else { 
+        long long int count = 0;
+        long double N_ = N;
+        for(int fc = 2; fc < sqrt(N_); fc ++) {
+            if(N % fc == 0) count ++;
         }
-    }
-    return res_count;
-}
-
-int is_prime(long int num) {
-    if(num == 2 || num == 3) return 1;
-    else {
-        double num_ = num;
-        for(int i = 2; i <= sqrt(num_) + 1; i ++) {
-            if(num % i == 0) return 0;
-        }
-        return 1;
+        return count * 2 + 2;
     }
 }
