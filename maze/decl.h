b8e51d812b2584d3379166ee36321c39acc58aa5
leondelee <leevecent@outlook.com>
2018-10-20 15:58:39 +0800

    maze

---
+++ maze/decl.h
@@ -0,0 +1,21 @@
+//
+// Created by llw on 18-10-20.
+//
+
+#ifndef DEVOIR_DECL_H
+#define DEVOIR_DECL_H
+
+<template T> class Queue {
+private:
+    int max_size;
+    int front;
+    int rear;
+    T* list_array;
+public:
+    Queue(int size);
+    ~Queue();
+    void enqueue(const T &element);
+    T delete_queue();
+    int length();
+};
+#endif DEVOIR_DECL_H
