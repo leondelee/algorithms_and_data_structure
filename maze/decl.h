9534cc934fd99a81228b9f9603d37f7660d52367
leondelee <leevecent@outlook.com>
2018-10-29 23:41:01 +0800

    maze

--- maze/decl.h
+++ maze/decl.h
@@ -4,6 +4,18 @@
 #ifndef DEVOIR_DECL_H
 #define DEVOIR_DECL_H
 
+enum Directions {
+    UP = 0,
+    DOWN = 1,
+    LEFT = 2,
+    RIGHT = 3
+};
+
+struct State {
+    int x, y;
+    Directions intention;
+};
+
 template <typename T> class Queue {
 public:
     int max_size;
@@ -15,5 +27,14 @@ public:
     T dequeue();
     int get_queue_length();
     void output_queue();
+};
+
+class Agent {
+private:
+    int maze_size;
+    int min_steps;
+    vector<Directions>
+public:
+    Queue<int>
 };
 #endif
