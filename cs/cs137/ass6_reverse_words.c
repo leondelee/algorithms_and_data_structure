1acb016acdf983cfd8b78738d681bdadfe872f9e
leondelee <leevecent@outlook.com>
2018-10-31 12:20:30 +0800

    ass6

---
+++ cs/cs137/ass6_reverse_words.c
@@ -0,0 +1,42 @@
+//
+// Created by llw on 18-10-31.
+//
+#include <stdio.h>
+#include <assert.h>
+
+void reverse_word(char* word, int word_length);
+void deal_line();
+
+int main() {
+    int num_of_test_case = 0;
+    scanf("%d", &num_of_test_case);
+    getchar();
+    for(int ntc = 0; ntc < num_of_test_case; ntc ++) {
+        deal_line();
+    }
+    return 0;
+}
+
+void reverse_word(char* word, int word_length) {
+    assert(word_length >= 1);
+    for(int i = 0; i < word_length; i++) {
+        printf("%c", word[word_length - i - 1]);
+    }
+}
+
+void deal_line() {
+    int word_length = 0;
+    char current_ch;
+    char current_word[1000];
+    while(scanf("%c", &current_ch)) {
+        current_word[word_length] = current_ch;
+        word_length ++;
+        if(current_ch == ' ') {
+            reverse_word(current_word, word_length);
+            word_length = 0;
+        }
+        if(current_ch == '\n') break;
+    }
+}
+
+
