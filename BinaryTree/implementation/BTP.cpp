5f9bd381dbe2a48d84fc5bcfc6ae55e7e60ab8d7
speit <liliangwei@sjtu.edu.cn>
2018-11-10 21:31:06 +0800

    binary tree

--- BinaryTree/implementation/BTP.cpp
+++ BinaryTree/implementation/BTP.cpp
@@ -9,8 +9,8 @@ class BinaryTree {
 private:
     Key key;
     E value;
-    BinaryTree* left_node_ptr;
-    BinaryTree* right_node_ptr;
+    BinaryTree<Key, E>* left_node_ptr;
+    BinaryTree<Key, E>* right_node_ptr;
 public:
     BinaryTree() {
         left_node_ptr = nullptr;
@@ -25,9 +25,14 @@ public:
 
     E get_value() const { return value;}
     void set_value(const E& element) { value = element;}
-    BinaryTree* get_left_node_ptr() const { return left_node_ptr;}
-    void set_left_node_ptr(const BinaryTree* arg_left_node_ptr) { left_node_ptr = arg_left_node_ptr;}
-    BinaryTree* get_right_node_ptr() const { return right_node_ptr;}
-    void set_right_node_ptr(const BinaryTree* arg_right_node_ptr) { right_node_ptr = arg_right_node_ptr;}
+    BinaryTree<Key, E>* get_left_node_ptr() const { return left_node_ptr;}
+    void set_left_node_ptr(BinaryTree<Key, E>* arg_left_node_ptr) { left_node_ptr = arg_left_node_ptr;}
+    BinaryTree<Key, E>* get_right_node_ptr() const { return right_node_ptr;}
+    void set_right_node_ptr(BinaryTree<Key, E>* arg_right_node_ptr) { right_node_ptr = arg_right_node_ptr;}
     bool is_leaf() { return (left_node_ptr == nullptr) && (right_node_ptr == nullptr);}
+
+    // overloading new and delete
+//    void *operator new(size_t) {
+//        return ::new BinaryTree<Key, E>;
+//    }
 };
--- BinaryTree/test.cpp
+++ BinaryTree/test.cpp
@@ -3,6 +3,8 @@
 //
 #include <iostream>
 
+#include "implementation/BTP.cpp"
+
 using namespace std;
 
 class Test {
@@ -12,7 +14,10 @@ public:
 };
 
 int main() {
-    int* a;
-    bool b = (a == nullptr);
-    cout << b << endl;
+    BinaryTree<int, int>* tree = new BinaryTree<int, int>[5];
+    BinaryTree<int, int> tree1 = *(tree + 1);
+    BinaryTree<int, int>* ptr = & tree1;
+    tree1.set_left_node_ptr(ptr);
+    cout << tree1.get_left_node_ptr() << endl;
+    cout << tree1.get_right_node_ptr() << endl;
 }
