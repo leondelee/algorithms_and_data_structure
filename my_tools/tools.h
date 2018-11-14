9d5494a22096628cb9f8f081407fd575edd7272a
speit <liliangwei@sjtu.edu.cn>
2018-11-14 13:00:40 +0800

    define some useful tools

---
+++ my_tools/tools.h
@@ -0,0 +1,17 @@
+//
+// Created by speit on 18-11-14.
+//
+
+#ifndef DEVOIR_TOOLS_H
+#define DEVOIR_TOOLS_H
+
+#include <iostream>
+
+using namespace std;
+
+template <typename T>
+void my_cout(T message) {
+    cout << message << endl;
+}
+
+#endif //DEVOIR_TOOLS_H
