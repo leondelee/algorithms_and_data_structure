59aece10ec1c05b486066f645336c668549e9b67
speit <liliangwei@sjtu.edu.cn>
2018-11-21 21:51:56 +0800

    add c tools

---
+++ cs/cs137/c_tools/debug_tools.h
@@ -0,0 +1,15 @@
+//
+// Created by speit on 18-11-21.
+//
+
+#ifndef DEVOIR_DEBUG_TOOLS_H
+#define DEVOIR_DEBUG_TOOLS_H
+
+#include <stdio.h>
+#include <string.h>
+
+void my_print(char* message) {
+    printf("\n%s", message);
+}
+
+#endif //DEVOIR_DEBUG_TOOLS_H
