f4d868de6cc7596c81f7ba008af6f5e1654f546c
leondelee <leevecent@outlook.com>
2018-12-08 22:41:09 +0800

    find clostest

--- cs/cs137/ass8_find_closest.c
+++ cs/cs137/ass8_find_closest.c
@@ -2,3 +2,14 @@
 // Created by llw on 18-12-5.
 //
 
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    int num_cases;
+    scanf("%d", & num_cases);
+    for(int i = 0; i < num_cases; i ++) {
+
+    }
+}
+
